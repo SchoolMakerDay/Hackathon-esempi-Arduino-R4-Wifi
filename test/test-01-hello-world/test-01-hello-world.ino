9a0480dde867f2c346506bfa91957f14e3b76128
SchoolMakerDay <50214407+SchoolMakerDay@users.noreply.github.com>
2025-02-20 14:47:40 +0100

    Add files via upload

---
+++ test/test-01-hello-world/test-01-hello-world.ino
@@ -0,0 +1,18 @@
+/*
+Hackathon 2025
+Secondaria II grado
+TEST01: salve mondo
+test di funzionalità della scheda WEMOS LOLIN R1 D32.
+Fa lampeggiare il LED integrato nella scheda collegato alla porta LED_BUILTIN
+*/
+#include "wemosd1r32.h"
+void setup() {
+pinMode(LED_BUILTIN, OUTPUT);     //Inzializza la porta 2 come uscita
+}
+
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);  //Accende il LED
+  delay(2000);                      //Aspetta 2 secondi
+  digitalWrite(LED_BUILTIN, LOW);   //Spegne il LED
+  delay(1000);                      //aspetta 1 secondo
+}
